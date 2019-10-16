76a6e372981112095a1280a1f582a5bff7cb1f21
Sid Lin <sydney.lin12@gmail.com>
2019-10-16 17:19:11 -0500

    Made the base for the kilo calculator app.

---
+++ KiloCalc/KiloCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KiloCalc
+//
+//  Created by Sydney Lin on 10/16/19.
+//  Copyright © 2019 BroGains Brothers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // Override the status bar text color to white
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+
+
+}
+
