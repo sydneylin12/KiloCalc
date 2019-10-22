84af4c1dc0abee2e30ca3e6a40b61824ab675855
Sid Lin <sydney.lin12@gmail.com>
2019-10-22 18:02:27 -0500

    Got the buttons and clear button working

--- KiloCalc/KiloCalc/ViewController.swift
+++ KiloCalc/KiloCalc/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Label for the text
+    @IBOutlet weak var resultLabel: UILabel!
+    // Private variables/fields equivalent
+    var defaultWeight: Double = 20
+    var weightOnBar: Double = 20
+    var canClear = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -20,6 +26,32 @@ class ViewController: UIViewController {
         return .lightContent
     }
 
-
+    // Button send event
+    @IBAction func handleButtonPress(_ sender: UIButton) {
+        // Clears the calculator
+        if canClear {
+            resultLabel.text = String(weightOnBar)
+            weightOnBar = 20
+            canClear = false
+        }
+        print(sender.titleLabel?.text)
+        // Format the double
+        var addString = sender.titleLabel?.text
+        //Need the exclamation mark for some reason
+        var addDouble = Double(addString!)
+        weightOnBar = calculate(weightOnBar, addDouble!)
+        resultLabel.text = String(weightOnBar)
+    }
+    
+    // Clear out the calculator - reset to 20 kg
+    @IBAction func clearCalculator(_ sender: UIButton) {
+        weightOnBar = 20
+        resultLabel.text = String(weightOnBar)
+    }
+    
+    func calculate(_ a: Double, _ b: Double) -> Double {
+        return a + b
+    }
+    
 }
 
