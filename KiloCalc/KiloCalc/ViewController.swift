e8059a7e557617ce1b916eaeb490ec2abdbc266a
Sid Lin <sydney.lin12@gmail.com>
2019-10-23 16:08:24 -0500

    Finished up changes and fixed iPhone X.. UI

--- KiloCalc/KiloCalc/ViewController.swift
+++ KiloCalc/KiloCalc/ViewController.swift
@@ -9,16 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //Label for the text
-    @IBOutlet weak var resultLabel: UILabel!
-    // Private variables/fields equivalent
-    var defaultWeight: Double = 20
-    var weightOnBar: Double = 20
-    var canClear = true
+    //Label for the kilogram and pound text
+    @IBOutlet weak var kgLabel: UILabel!
+    @IBOutlet weak var lbLabel: UILabel!
     
+    // Default weight does not need to be changed
+    let defaultWeight: Double = 20.0
+    // List of weights on the bar
+    var weightList: [Double] = [20.0]
+    
+    // There by default
     override func viewDidLoad() {
+        // Initialize the calculator
+        updateText()
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     // Override the status bar text color to white
@@ -28,29 +32,65 @@ class ViewController: UIViewController {
 
     // Button send event
     @IBAction func handleButtonPress(_ sender: UIButton) {
-        // Clears the calculator
-        if canClear {
-            resultLabel.text = String(weightOnBar)
-            weightOnBar = 20
-            canClear = false
-        }
-        print(sender.titleLabel?.text)
         // Format the double
-        var addString = sender.titleLabel?.text
-        //Need the exclamation mark for some reason
-        var addDouble = Double(addString!)
-        weightOnBar = calculate(weightOnBar, addDouble!)
-        resultLabel.text = String(weightOnBar)
+        let addString = sender.titleLabel?.text
+        //Need the exclamation to unwrap the nullable/optional double
+        let addDouble = Double(addString!)
+        // Append the button click to the total list
+        weightList.append(addDouble!)
+        // Debug
+        print(weightList)
+        updateText()
     }
     
     // Clear out the calculator - reset to 20 kg
     @IBAction func clearCalculator(_ sender: UIButton) {
-        weightOnBar = 20
-        resultLabel.text = String(weightOnBar)
+        weightList = [defaultWeight]
+        updateText()
     }
     
+    // Remove the last element of the list
+    @IBAction func removeButton(_ sender: UIButton) {
+        remove()
+    }
+    
+    // Update the text of both labels
+    func updateText(){
+        let total = calculateList(list: weightList)
+        print(total)
+        let totalKg = String(format: "%.2f", total)
+        let totalLb = String(format: "%.2f", total * 2.2)
+        kgLabel.text = "\(totalKg) kg"
+        lbLabel.text = "\(totalLb) lbs"
+    }
+    
+    // Add 2 doubles and return the result
     func calculate(_ a: Double, _ b: Double) -> Double {
         return a + b
+    }
+    
+    func remove() -> Void {
+        if(weightList.count == 1){
+            print("Cannot remove any plates.")
+            return
+        }
+        else{
+            print("Removing last element.")
+            weightList.removeLast()
+            updateText()
+        }
+    }
+    
+    // Iterate through a list and sum it up into a double
+    func calculateList(list: [Double]) -> Double {
+        var total: Double = 0.0
+        total += list[0] //Add the bar at index 0
+        for i in 1 ..< list.count {
+            // Every plate must be added twice
+            //print(list[i])
+            total += 2 * list[i]
+        }
+        return total
     }
     
 }
