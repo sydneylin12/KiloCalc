3971b7a178987560353978bfe3465f2012dad97d
Sid Lin <sydney.lin12@gmail.com>
2019-12-25 18:33:42 -0600

    Added collar toggle option

--- KiloCalc/KiloCalc/ViewController.swift
+++ KiloCalc/KiloCalc/ViewController.swift
@@ -12,11 +12,19 @@ class ViewController: UIViewController {
     //Label for the kilogram and pound text
     @IBOutlet weak var kgLabel: UILabel!
     @IBOutlet weak var lbLabel: UILabel!
+    @IBOutlet weak var collarButton: UIButton!
     
     // Default weight does not need to be changed
     let defaultWeight: Double = 20.0
+    
+    // Collar weight constant
+    let collarWeight: Double = 5.0
+    
     // List of weights on the bar
     var weightList: [Double] = [20.0]
+    
+    // Indicates if the collars are on
+    var hasCollar: Bool = false
     
     // There by default
     override func viewDidLoad() {
@@ -45,7 +53,12 @@ class ViewController: UIViewController {
     
     // Clear out the calculator - reset to 20 kg
     @IBAction func clearCalculator(_ sender: UIButton) {
+        // Reset to just the bar
         weightList = [defaultWeight]
+        // Reset the collar
+        collarButton.setImage(UIImage(named: "Collar"), for: .normal)
+        hasCollar = false
+        // Update text after clearing
         updateText()
     }
     
@@ -54,24 +67,41 @@ class ViewController: UIViewController {
         remove()
     }
     
+    // Call function when collar is clicked
+    @IBAction func onCollarClicked(_ sender: UIButton){
+        // If not toggled
+        if(!hasCollar){
+            // Update to highlighted image
+            sender.setImage(UIImage(named: "DarkCollar"), for: .normal)
+            weightList.insert(collarWeight, at: 1)
+        }
+        // If toggled
+        else{
+            // Update image to normal again
+            sender.setImage(UIImage(named: "Collar"), for: .normal)
+            weightList.remove(at: 1) //Remove at index 1
+        }
+        hasCollar = !hasCollar
+        updateText()
+    }
+    
     // Update the text of both labels
     func updateText(){
         let total = calculateList(list: weightList)
-        print(total)
         let totalKg = String(format: "%.2f", total)
         let totalLb = String(format: "%.2f", total * 2.2)
         kgLabel.text = "\(totalKg) kg"
         lbLabel.text = "\(totalLb) lbs"
     }
     
-    // Add 2 doubles and return the result
-    func calculate(_ a: Double, _ b: Double) -> Double {
-        return a + b
-    }
-    
     func remove() -> Void {
-        if(weightList.count == 1){
+        //If there are no collars and only 1 plate
+        if(weightList.count == 1 && !hasCollar){
             print("Cannot remove any plates.")
+            return
+        }
+        else if(weightList.count == 2 && hasCollar){
+            print("Use toggle to remove collars!")
             return
         }
         else{
