addd7ab065bd95393a51f83e4c94c0667fbca90d
Sid Lin <sydney.lin12@gmail.com>
2020-03-31 22:06:10 -0500

    Backup

---
+++ KiloCalc/KiloCalc/PlateNode.swift
@@ -0,0 +1,30 @@
+//
+//  PlateNode.swift
+//  KiloCalc
+//
+//  Created by Sydney Lin on 3/31/20.
+//  Copyright © 2020 BroGains Brothers. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlateNode {
+    var weight: Double
+    var color: UIColor
+    var textColor: UIColor
+    
+    // Use white as the default color
+    init(weight: Double, color: UIColor) {
+        self.weight = weight
+        self.color = color
+        self.textColor = UIColor.white
+    }
+    
+    // Class for the weight objects in the list
+    init(weight: Double, color: UIColor, textColor: UIColor) {
+        self.weight = weight
+        self.color = color
+        self.textColor = textColor
+    }
+}
--- KiloCalc/KiloCalc/ViewController.swift
+++ KiloCalc/KiloCalc/ViewController.swift
@@ -11,42 +11,33 @@ import AVFoundation
 
 class ViewController: UIViewController {
     //Label for the kilogram and pound text
-    @IBOutlet weak var kgLabel: UILabel!
-    @IBOutlet weak var lbLabel: UILabel!
+    @IBOutlet weak var lbs: UILabel!
+    @IBOutlet weak var kgs: UILabel!
     @IBOutlet weak var collarButton: UIButton!
+    @IBOutlet weak var visual: UIView!
     
     // Default weight does not need to be changed
     let defaultWeight: Double = 20.0
     
     // Collar weight constant
-    let collarWeight: Double = 5.0
+    let collarWeight: Double = 2.5
     
     // List of weights on the bar
     var weightList: [Double] = [20.0]
+    var plateList: [PlateNode] = []
+    
+    let maxPlates: Int = 10
     
     // Indicates if the collars are on
     var hasCollar: Bool = false
     
-    // For button audio
-    var audioPlayer: AVAudioPlayer = AVAudioPlayer()
-    
-    // There by default
+    // Initialize the calculator
     override func viewDidLoad() {
-        // Initialize the calculator
-        updateText()
-        
-        // Instantiate audio player with click sound
-        let click = Bundle.main.path(forResource: "Click", ofType: ".mp3")
-        
-        // Swift try-catch block
-        do{
-            try audioPlayer = AVAudioPlayer(contentsOf: URL(fileURLWithPath: click!))
-        }
-        catch{
-            print("Error constructing audio.")
-        }
-        
         super.viewDidLoad()
+        lbs.font = UIFont.monospacedDigitSystemFont(ofSize: 36, weight: UIFont.Weight.regular)
+        kgs.font = UIFont.monospacedDigitSystemFont(ofSize: 36, weight: UIFont.Weight.regular)
+        updateText()
+        generatePlateVisual()
     }
     
     // Override the status bar text color to white
@@ -56,94 +47,130 @@ class ViewController: UIViewController {
 
     // Button send event
     @IBAction func handleButtonPress(_ sender: UIButton) {
-        // Play clicked audio
-        audioPlayer.play()
-        // Format the double
-        let addString = sender.titleLabel?.text
-        //Need the exclamation to unwrap the nullable/optional double
-        let addDouble = Double(addString!)
-        // Append the button click to the total list
-        weightList.append(addDouble!)
-        // Debug
-        print(weightList)
+        
+        if plateList.count >= 10 {
+            print("Cannot add any more plates!")
+            return
+        }
+        
+        let addString: String = (sender.titleLabel?.text)!
+        let addDouble: Double = Double(addString)!
+        switch addDouble {
+        case 25.0:
+            plateList.append(PlateNode(weight: 25.0, color: UIColor.red))
+        case 20.0:
+            plateList.append(PlateNode(weight: 20.0, color: UIColor.blue))
+        case 15.0:
+            plateList.append(PlateNode(weight: 15.0, color: UIColor.systemYellow))
+        case 10.0:
+            plateList.append(PlateNode(weight: 10.0, color: UIColor.green))
+        case 5.0:
+            plateList.append(PlateNode(weight: 5.0, color: UIColor.white, textColor: UIColor.black))
+        case 2.5:
+            plateList.append(PlateNode(weight: 2.5, color: UIColor.black))
+        case 1.25:
+            plateList.append(PlateNode(weight: 1.25, color: UIColor.systemGray))
+        case 0.5:
+            plateList.append(PlateNode(weight: 0.5, color: UIColor.systemGray2))
+        case 0.25:
+            plateList.append(PlateNode(weight: 0.25, color: UIColor.systemGray3))
+        default:
+            print("Error creating plate node!")
+        }
         updateText()
+        generatePlateVisual()
     }
     
     // Clear out the calculator - reset to 20 kg
     @IBAction func clearCalculator(_ sender: UIButton) {
-        // Play clicked audio
-        audioPlayer.play()
         // Reset to just the bar
-        weightList = [defaultWeight]
-        // Reset the collar
-        collarButton.setImage(UIImage(named: "Collar"), for: .normal)
+        plateList = []
         hasCollar = false
-        // Update text after clearing
         updateText()
+        
+        // Clear out the super view
+        for sub in visual.subviews {
+            sub.removeFromSuperview()
+        }
     }
     
     // Remove the last element of the list
     @IBAction func removeButton(_ sender: UIButton) {
-        // Play clicked audio
-        audioPlayer.play()
-        remove()
+        //If there are no collars and only 1 plate
+        if plateList.count == 0 {
+            print("Cannot remove any more plates.")
+            return
+        }
+        print("Removing last element.")
+        plateList.removeLast()
+        updateText()
     }
     
     // Call function when collar is clicked
     @IBAction func onCollarClicked(_ sender: UIButton){
-        // Play clicked audio
-        audioPlayer.play()
-
-        // If not toggled
-        if(!hasCollar){
-            // Update to highlighted image
-            sender.setImage(UIImage(named: "DarkCollar"), for: .normal)
+        if !hasCollar {
             weightList.insert(collarWeight, at: 1)
         }
-        // If toggled
-        else{
-            // Update image to normal again
-            sender.setImage(UIImage(named: "Collar"), for: .normal)
+        else {
             weightList.remove(at: 1) //Remove at index 1
         }
         hasCollar = !hasCollar
         updateText()
     }
     
-    // Update the text of both labels
-    func updateText() -> Void {
-        let total = calculateList(list: weightList)
-        let totalKg = String(format: "%.2f", total)
-        let totalLb = String(format: "%.2f", total * 2.2)
-        kgLabel.text = "\(totalKg) kg"
-        lbLabel.text = "\(totalLb) lbs"
+    // Create the plates indicator
+    func generatePlateVisual() {
+        if plateList.count <= 0 {
+            return // prevent errors
+        }
+        
+        // Sort in descending order
+        plateList.sort(by: { $0.weight > $1.weight })
+        
+        let plateWidth = visual.layer.frame.width / 10
+        let plateHeight = visual.layer.frame.height
+        var xCenter = plateWidth / 2
+        let yCenter = plateHeight / 2
+        
+        for plate in plateList {
+            let frame: CGRect = CGRect(x: 0, y: 0, width: plateWidth, height: plateHeight)
+            let tile: UILabel = UILabel(frame: frame)
+            
+            // Style label
+            tile.layer.backgroundColor = plate.color.cgColor
+            //tile.layer.borderColor = UIColor.black.cgColor
+            tile.layer.borderColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1).cgColor
+            tile.layer.cornerRadius = 5.0
+            tile.layer.masksToBounds = true
+            tile.layer.borderWidth = 2.0
+            
+            // Create text for label
+            tile.text = String(plate.weight)
+            tile.textColor = plate.textColor
+            tile.textAlignment = .center
+
+            // Add via center
+            tile.center = CGPoint(x: xCenter, y: yCenter)
+            visual.addSubview(tile)
+            xCenter += plateWidth
+        }
     }
     
-    func remove() -> Void {
-        //If there are no collars and only 1 plate
-        if(weightList.count == 1 && !hasCollar){
-            print("Cannot remove any plates.")
-            return
-        }
-        else if(weightList.count == 2 && hasCollar){
-            print("Use toggle to remove collars!")
-            return
-        }
-        else{
-            print("Removing last element.")
-            weightList.removeLast()
-            updateText()
-        }
+    // Update the text of both labels
+    func updateText() {
+        let total = calculateList(list: weightList)
+        let totalKg = String(format: "%.2f", total)
+        let totalLbs = String(format: "%.2f", total * 2.2)
+        kgs.text = "\(totalKg) kg"
+        lbs.text = "\(totalLbs) lbs"
     }
     
     // Iterate through a list and sum it up into a double
     func calculateList(list: [Double]) -> Double {
         var total: Double = 0.0
         total += list[0] //Add the bar at index 0
-        for i in 1 ..< list.count {
-            // Every plate must be added twice
-            //print(list[i])
-            total += 2 * list[i]
+        for plate in plateList {
+            total += 2 * plate.weight
         }
         return total
     }
